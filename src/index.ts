c44f6cb61c0eef9162dc3672c148f8859aedd3b5
Mitu217 <mtksk.26884um@gmail.com>
2018-07-26 02:07:05 +0900

    Add cron job

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 'use strict';
 
-import {createApp} from './triggers';
+import {CronJob} from 'cron';
+import * as express from 'express';
+import {scraping} from './haruka';
+
+const createApp = () => {
+    // setup cron job
+    const job = new CronJob('0 * * * * *', () => {
+        scraping()
+    });
+    job.start();
+
+    // setup application
+    let app = express();
+    app.get('/haruka', scrapingAction);
+    return app;
+}
+
+const scrapingAction = async (request: any, response: any) => {
+    const releaseItems = await scraping();
+    return response.json({ music: releaseItems })
+};
 
 const app = createApp();
 app.listen(8080);
