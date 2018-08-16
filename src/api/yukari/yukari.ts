33ef91baeb8fd5b3b104a7c216e9c07333db4081
Mitu217 <mtksk.26884um@gmail.com>
2018-08-16 09:28:25 +0900

    fix: support invalid date string

--- src/api/yukari/yukari.ts
+++ src/api/yukari/yukari.ts
@@ -4,12 +4,13 @@ import * as uuid from 'node-uuid';
 export const exportCalendarAction = async (req: any, res: any) => {
     // get current data
     const date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const timestamp = date.toISOString().replace(/[-:]/g, '').replace(/\.\d+/g, '');
 
-    const prevMonthEvents = await getEvents(month == 1 ? year - 1 : year, month == 1 ? 12 : month - 1);
-    const currentMonthEvents = await getEvents(year, month);
-    const nextMonthEvents = await getEvents(month == 12 ? year + 1 : year, month == 12 ? 1 : month + 1);
+    const prevMonthEvents = await getEvents(month == 1 ? year - 1 : year, month == 1 ? 12 : month - 1, timestamp);
+    const currentMonthEvents = await getEvents(year, month, timestamp);
+    const nextMonthEvents = await getEvents(month == 12 ? year + 1 : year, month == 12 ? 1 : month + 1, timestamp);
 
     const events = prevMonthEvents + currentMonthEvents + nextMonthEvents;
     let result =`BEGIN:VCALENDAR
@@ -17,30 +18,29 @@ PRODID:-//ortensia/ ICS//EN
 VERSION:2.0
 CALSCALE:GREGORIAN
 METHOD:PUBLISH
-X-WR-CALNAME:CDReleaseCalendar
+X-WR-CALNAME:CD Release Calendar
 X-WR-TIMEZONE:Asia/Tokyo
 ${events}END:VCALENDAR`
     return res.send(result);
 }
 
-export const getEvents = async (year: number, month: number): Promise<string> => {
-    const date = new Date();
-    const dateString = date.toISOString().replace(/[-:.]/g, '');
+export const getEvents = async (year: number, month: number, timestamp: string): Promise<string> => {
     const cache = await readCache(year, month);
     let events = "";
     cache.forEach((c : any) => {
         const uid = uuid.v4();
         const name = c.name;
-        const startdate = Number(c.date.replace(/-/g, ''));
-        const enddate = startdate + 1;
-        events += `BEGIN:VEVENT
-DTSTART;VALUE=DATE:${startdate}
-DTEND;VALUE=DATE:${enddate}
-DTSTAMP:${dateString}
+        const startDate = Number(c.date.replace(/-/g, ''));
+        if (!isNaN(startDate)) {
+            const endDate = startDate + 1;
+            events += `BEGIN:VEVENT
+DTSTART;VALUE=DATE:${startDate}
+DTEND;VALUE=DATE:${endDate}
+DTSTAMP:${timestamp}
 UID:${uid}
-CREATED:${dateString}
+CREATED:${timestamp}
 DESCRIPTION:${name}
-LAST-MODIFIED:${dateString}
+LAST-MODIFIED:${timestamp}
 LOCATION:
 SEQUENCE:1
 STATUS:CONFIRMED
@@ -48,6 +48,7 @@ SUMMARY:${name}
 TRANSP:OPAQUE
 END:VEVENT
 `
+        }
     });
     return events;
 }
